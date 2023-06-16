4ac1ff06230e868cf9d74dea1aca037b8a542252
IoaMertz <119110449+IoaMertz@users.noreply.github.com>
2023-06-16 13:06:12 +0300

    continuingthe tutorial

--- Mutex/Program.cs
+++ Mutex/Program.cs
@@ -11,30 +11,33 @@ namespace MutexSection
 
             public void Deposite(int amount)
             {
-                
                     Balance += amount;
-                
             }
 
             public void Withdraw(int amount)
             {
-                
                     Balance -= amount;
-                
+            }
 
+            public void Transfer(BankAccount where, int amount)
+            {
+                Balance -= amount;
+                where.Balance += amount;
             }
         }
-        public static void kati()
-        {
-            Mutex mutex = new Mutex();
-        }
+        
 
         static void Main(string[] args)
         {
             var tasks = new List<Task>();
             var ba = new BankAccount();
+            var ba2 = new BankAccount();
 
+
+            //is a construct that controlles accces to an area of code.
             Mutex mutex = new Mutex();
+            Mutex mutex2 = new Mutex();
+            
 
 
 
@@ -44,23 +47,69 @@ namespace MutexSection
                 {
                     for (int j = 0; j < 1000; j++)
                     {
-                        ba.Deposite(100);
+                        // waiting for the mutex to become available
+                        bool havelock = mutex.WaitOne();
+                        try
+                        {
+                            ba.Deposite(1);
+                        }
+                        finally
+                        {
+                            if(havelock) mutex.ReleaseMutex();
+                        }
+
                     }
                 }));
                 tasks.Add(Task.Factory.StartNew(() =>
                 {
                     for (int j = 0; j < 1000; j++)
                     {
-                        ba.Withdraw(100);
+                        bool havelock = mutex2.WaitOne();
+                        try
+                        {
+                            ba2.Deposite(1);
+                        }
+                        finally
+                        {
+                            if (havelock)
+                            {
+                                mutex2.ReleaseMutex();
+                            }
+
+                        }
                     }
                 }));
+
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        //we want to use both accounts so we need to use both mutex
+                        // this is the same as waitHandle.WaitAll()
+                        bool haveLock = Mutex.WaitAll(new[] { mutex,mutex2});
+                        try
+                        {
+                            ba.Transfer(ba2,1);
+                        }
+                        finally
+                        {
+                            if (haveLock)
+                            {
+                                mutex.ReleaseMutex();
+                                mutex2.ReleaseMutex();
+
+                            }
+                        }
+
+                    }
+                }));
+
+
             }
 
 
 
             //the result isnt 0 as we might expect but in varies.
-
-
             //+= is not an operation but 2.Is not atomic
             // op1: temp < -get_Balance() + amount
             // op2: set_Balance(temp)
@@ -70,6 +119,7 @@ namespace MutexSection
 
             Task.WaitAll(tasks.ToArray());
             Console.WriteLine($"Final balance is {ba.Balance}");
+            Console.WriteLine($"Final balance is {ba2.Balance}");
 
         }
     }
---
+++ Read-Writer-Locks/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Read_Writer_Locks
+{
+    internal class Program
+    {
+        static ReaderWriterLockSlim padlock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            var tasks = new List<Task>();
+            for(int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    padlock.EnterReadLock();
+                    padlock.EnterReadLock();
+                    Console.WriteLine($"Entered read lock, x = {0}");
+                    Thread.Sleep(5000);
+
+                    padlock.ExitReadLock();
+                    padlock.ExitReadLock();
+                    Console.WriteLine($"Exited read lock, x= {x}.");
+                }));
+            }
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    Console.WriteLine(e);
+                    return true;
+                });
+            }
+            while (true)
+            {
+                Console.ReadKey();
+                padlock.EnterWriteLock();
+                Console.WriteLine("Write Lock Aquired");
+                int newValue = random.Next(10);
+                x= newValue;
+                Console.WriteLine($"set x = {x}");
+                padlock.ExitWriteLock();
+                Console.WriteLine("Write lock released");
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
