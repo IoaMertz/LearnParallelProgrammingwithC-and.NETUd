c6324bf0d59497708ad41dbe1e0b8acf67120303
IoaMertz <119110449+IoaMertz@users.noreply.github.com>
2023-06-16 17:57:02 +0300

    xx

---
+++ Continuations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Continuations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var task = Task.Factory.StartNew(() =>
+            //{
+            //    Console.WriteLine("Boiling Water");
+            //});
+
+            //var task2 = task.ContinueWith(t =>
+            //{
+            //    Console.WriteLine($"Completed task {t.Id}, pour water into cup.");
+            //});
+
+            //task2.Wait();
+
+            var task = Task.Factory.StartNew(() => "Task 1");
+            var task2 = Task.Factory.StartNew(() => "Task 2");
+
+            var task3 = Task.Factory.ContinueWhenAll(new[] { task,task2},tasks =>
+            {
+                Console.WriteLine("Tasks completed:");
+                foreach (var task in tasks) 
+                {
+                  Console.WriteLine("-" + task.Result);
+
+                }
+                Console.WriteLine("All tasks done");
+            });
+
+            task3.Wait();
+
+
+        }
+    }
+}
